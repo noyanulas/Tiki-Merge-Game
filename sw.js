0bbfbc35121a1bb16eaec4728350d9388480b374
Ulaş Noyan <99445511+noyanulas@users.noreply.github.com>
2026-06-30 15:04:55 +0300

    Tiki Arcade HUB

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
-// Makes Tiki Merge playable with NO internet after the very first visit.
-const CACHE = 'tiki-merge-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest',
-                './icon-180.png', './icon-192.png', './icon-512.png'];
+// Caches the whole arcade (hub + every game) for offline play after the first visit.
+const CACHE = 'tiki-arcade-v1';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest',
+  './icon-180.png', './icon-192.png', './icon-512.png',
+  './merge/', './merge/index.html',
+  './tycoon/', './tycoon/index.html'
+];
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
 });
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys()
-      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
